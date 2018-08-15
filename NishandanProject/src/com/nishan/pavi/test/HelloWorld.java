fe1786a3f2b6c4fca86a4a91d905f14ad538c859
Nishandan <nishanuom@gmail.com>
2018-08-15 19:11:22 +0800

    Hello World

---
+++ NishandanProject/src/com/nishan/pavi/test/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.nishan.pavi.test;
+
+/**
+ * @author WCCTTI-NISHAN
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("Kanapathy ...!");
+
+	}
+
+}
