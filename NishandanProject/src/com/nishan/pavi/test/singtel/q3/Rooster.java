c4f40b6c21c3cfd9f1710647477f21d3dd7e96b8
Nishandan <nishanuom@gmail.com>
2018-08-19 22:27:34 +0800

    A.3

---
+++ NishandanProject/src/com/nishan/pavi/test/singtel/q3/Rooster.java
@@ -0,0 +1,12 @@
+package com.nishan.pavi.test.singtel.q3;
+
+import com.nishan.pavi.test.singtel.q2.Chicken;
+
+public class Rooster extends Chicken {
+
+	@Override
+	public void sing() {
+		System.out.println("Cock-a-doodle-doo");
+	}
+
+}
